aa95fadf4a78e0e0ba6bd18de0fc43bb99de5a0e
ilsang <ilsangbot@gmail.com>
2020-06-05 14:40:46 +0900

    브랜치 마지막 테스트

--- discordbot.py
+++ discordbot.py
@@ -2991,4 +2991,4 @@ while True:
 	print("Bot restarting")
 	client = discord.Client(loop=client.loop)
 	client = commands.Bot(command_prefix="", help_command = None, description='일상디코봇')
-	# 브랜치 변경
+	# 일상브랜치 마지막
